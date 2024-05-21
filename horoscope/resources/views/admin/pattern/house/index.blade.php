09bd7dfcc7d229dd97d22dbe8da4fe2efdd70d57
LamQuangTruong <lqtruong321@gmail.com>
2024-05-21 08:19:58 +0700

    Fix view table and re-seed data

--- horoscope/resources/views/admin/pattern/house/index.blade.php
+++ horoscope/resources/views/admin/pattern/house/index.blade.php
@@ -39,60 +39,62 @@
             </div>
         </div>
     </form>
-
-    <x-admin.table>
-        <x-slot name='header'>
-            <tr>
-                <th class="text-center"style="width: 5%">No</th>
-                <th class="text-center" style="width: 8%">@lang('table.planet_name')</th>
-                <th class="text-center" style="width: 8%">@lang('table.house_name')</th>
-                <th class="text-center">@lang('table.content')</th>
-                <th class="text-center">@lang('table.content_en')</th>
-                <th class="text-center">@lang('table.content_solar')</th>
-                <th class="text-center">@lang('table.content_en')</th>
-                <th  class="text-center">@lang('table.action')</th>
-            </tr>
-        </x-slot>
-        <x-slot name='body'>
-            @if (isset($housePatterns) && !empty($housePatterns))
-                @foreach ($housePatterns as $index => $pattern)
-                    <tr>
-                        <td class="text-center">{{ $pattern->id}}</td>
-                        @if (!session()->has('lang_code') || session('lang_code') == 'ja')
-                            <td class="text-center">{{ $pattern->planet->name }}</td>
-                            <td class="text-center"> {{ $pattern->house->name }}</td>
-                        @else
-                            <td class="text-center"> {{ $pattern->planet->name_en }}</td>
-                            <td class="text-center">{{ $pattern->house->name_en }}</td>
-                        @endif
-                        <td class="text-left text-truncate" style="max-width: 320px">{!! nl2br(e($pattern->content)) !!}</td>
-                        <td class="text-left text-truncate" style="max-width: 320px">{!! nl2br(e($pattern->content_en)) !!}</td>
-                        <td class="text-left">{!! nl2br(e($pattern->content_solar)) !!}</td>
-                        <td class="text-left">{!! nl2br(e($pattern->content_solar_en)) !!}</td>
-                        <td class="text-center">
-                            <div class="btn-group">
-                            <a href={{ route('admin.pattern.house-view', $pattern->id) }}>
-                                <button type="button" class="btn btn-success mx-1">
-                                    <i class="bi bi-pencil-fill"></i>
+    
+    <div class="table-responsive">
+        <x-admin.table>
+            <x-slot name='header'>
+                <tr>
+                    <th class="text-center"style="width: 5%">No</th>
+                    <th class="text-center" style="width: 8%">@lang('table.planet_name')</th>
+                    <th class="text-center" style="width: 8%">@lang('table.house_name')</th>
+                    <th class="text-center">@lang('table.content')</th>
+                    <th class="text-center">@lang('table.content_en')</th>
+                    <th class="text-center">@lang('table.content_solar')</th>
+                    <th class="text-center">@lang('table.content_en')</th>
+                    <th  class="text-center">@lang('table.action')</th>
+                </tr>
+            </x-slot>
+            <x-slot name='body'>
+                @if (isset($housePatterns) && !empty($housePatterns))
+                    @foreach ($housePatterns as $index => $pattern)
+                        <tr>
+                            <td class="text-center">{{ $pattern->id}}</td>
+                            @if (!session()->has('lang_code') || session('lang_code') == 'ja')
+                                <td class="text-center">{{ $pattern->planet->name }}</td>
+                                <td class="text-center"> {{ $pattern->house->name }}</td>
+                            @else
+                                <td class="text-center"> {{ $pattern->planet->name_en }}</td>
+                                <td class="text-center">{{ $pattern->house->name_en }}</td>
+                            @endif
+                            <td class="text-left text-truncate" style="max-width: 320px">{!! nl2br(e($pattern->content)) !!}</td>
+                            <td class="text-left text-truncate" style="max-width: 320px">{!! nl2br(e($pattern->content_en)) !!}</td>
+                            <td class="text-left">{!! nl2br(e($pattern->content_solar)) !!}</td>
+                            <td class="text-left">{!! nl2br(e($pattern->content_solar_en)) !!}</td>
+                            <td class="text-center">
+                                <div class="btn-group">
+                                <a href={{ route('admin.pattern.house-view', $pattern->id) }}>
+                                    <button type="button" class="btn btn-success mx-1">
+                                        <i class="bi bi-pencil-fill"></i>
+                                    </button>
+                                </a>
+                                <button type="button" class="btn btn-danger mx-1" data-bs-toggle="modal"
+                                    data-bs-target="#deleteModal{{ $id = $pattern->id }}"
+                                    data-action={{ $action = route('admin.pattern.house-delete', $pattern->id) }}>
+                                    <i class="bi bi-x-lg"></i>
                                 </button>
-                            </a>
-                            <button type="button" class="btn btn-danger mx-1" data-bs-toggle="modal"
-                                data-bs-target="#deleteModal{{ $id = $pattern->id }}"
-                                data-action={{ $action = route('admin.pattern.house-delete', $pattern->id) }}>
-                                <i class="bi bi-x-lg"></i>
-                            </button>
-                            </div>
-                        </td>
+                                </div>
+                            </td>
 
-                    </tr>
-                    @include('Includes.admin.modal')
-                @endforeach
-            @endif
-        </x-slot>
-        <x-slot name='paginate'>
-            @include('Includes.admin.paginate', ['paginate' => $housePatterns])
-        </x-slot>
-    </x-admin.table>
+                        </tr>
+                        @include('Includes.admin.modal')
+                    @endforeach
+                @endif
+            </x-slot>
+            <x-slot name='paginate'>
+                @include('Includes.admin.paginate', ['paginate' => $housePatterns])
+            </x-slot>
+        </x-admin.table>
+    </div>
 </div>
 </div>
 </x-admin.layout>
--- horoscope/resources/views/admin/pattern/zodiac/index.blade.php
+++ horoscope/resources/views/admin/pattern/zodiac/index.blade.php
@@ -39,60 +39,62 @@
                     </div>
                 </div>
             </form>
-            <x-admin.table>
-                <x-slot name='header'>
-                    <tr>
-                        <th class="text-center" style="width: 5%">No</th>
-                        <th class="text-center" style="width: 8%">@lang('table.planet_name')</th>
-                        <th class="text-center" style="width: 8%">@lang('table.zodiac_name')</th>
-                        <th class="text-center">@lang('table.content')</th>
-                        <th class="text-center">@lang('table.content_en')</th>
-                        <th class="text-center">@lang('table.content_solar')</th>
-                        <th class="text-center">@lang('table.content_solar_en')</th>
-                        {{-- <th>@lang('title.published')</th> --}}
-                        <th class="text-center">@lang('table.action')</th>
-                    </tr>
-                </x-slot>
-                <x-slot name='body'>
-                    @if (isset($zodiacPatterns) && !empty($zodiacPatterns))
-                        @foreach ($zodiacPatterns as $index => $pattern)
-                            <tr>
-                                <td class="text-center">{{ $pattern->id }}</td>
-                                @if (!session()->has('lang_code') || session('lang_code') == 'ja')
-                                    <td class="text-center">{{ $pattern->planet->name }}</td>
-                                    <td class="text-center">{{ $pattern->zodiac->name }}</td>
-                                @else
-                                    <td class="text-center">{{ $pattern->planet->name_en }}</td>
-                                    <td class="text-center">{{ $pattern->zodiac->name_en }}</td>
-                                @endif
-                                <td class="text-left text-truncate" style="max-width: 320px">{!! nl2br(e($pattern->content)) !!}</td>
-                                <td class="text-left text-truncate" style="max-width: 320px">{!! nl2br(e($pattern->content_en)) !!}</td>
-                                <td class="text-left text-truncate" style="max-width: 320px">{!! nl2br(e($pattern->content_solar)) !!}</td>
-                                <td class="text-left text-truncate" style="max-width: 320px">{!! nl2br(e($pattern->content_solar_en)) !!}</td>
-                                <td class="text-center">
-                                    <div class="btn-group">
-                                        <a href={{ route('admin.pattern.zodiac-view', $pattern->id) }}>
-                                            <button type="button" class="btn btn-success mx-1">
-                                                <i class="bi bi-pencil-fill"></i>
+            <div class="table-responsive">
+                <x-admin.table>
+                    <x-slot name='header'>
+                        <tr>
+                            <th class="text-center" style="width: 5%">No</th>
+                            <th class="text-center" style="width: 8%">@lang('table.planet_name')</th>
+                            <th class="text-center" style="width: 8%">@lang('table.zodiac_name')</th>
+                            <th class="text-center">@lang('table.content')</th>
+                            <th class="text-center">@lang('table.content_en')</th>
+                            <th class="text-center">@lang('table.content_solar')</th>
+                            <th class="text-center">@lang('table.content_solar_en')</th>
+                            {{-- <th>@lang('title.published')</th> --}}
+                            <th class="text-center">@lang('table.action')</th>
+                        </tr>
+                    </x-slot>
+                    <x-slot name='body'>
+                        @if (isset($zodiacPatterns) && !empty($zodiacPatterns))
+                            @foreach ($zodiacPatterns as $index => $pattern)
+                                <tr>
+                                    <td class="text-center">{{ $pattern->id }}</td>
+                                    @if (!session()->has('lang_code') || session('lang_code') == 'ja')
+                                        <td class="text-center">{{ $pattern->planet->name }}</td>
+                                        <td class="text-center">{{ $pattern->zodiac->name }}</td>
+                                    @else
+                                        <td class="text-center">{{ $pattern->planet->name_en }}</td>
+                                        <td class="text-center">{{ $pattern->zodiac->name_en }}</td>
+                                    @endif
+                                    <td class="text-left text-truncate" style="max-width: 320px">{!! nl2br(e($pattern->content)) !!}</td>
+                                    <td class="text-left text-truncate" style="max-width: 320px">{!! nl2br(e($pattern->content_en)) !!}</td>
+                                    <td class="text-left text-truncate" style="max-width: 320px">{!! nl2br(e($pattern->content_solar)) !!}</td>
+                                    <td class="text-left text-truncate" style="max-width: 320px">{!! nl2br(e($pattern->content_solar_en)) !!}</td>
+                                    <td class="text-center">
+                                        <div class="btn-group">
+                                            <a href={{ route('admin.pattern.zodiac-view', $pattern->id) }}>
+                                                <button type="button" class="btn btn-success mx-1">
+                                                    <i class="bi bi-pencil-fill"></i>
+                                                </button>
+                                            </a>
+                                            <button type="button" class="btn btn-danger mx-1" data-bs-toggle="modal"
+                                                data-bs-target="#deleteModal{{ $id = $pattern->id }}"
+                                                data-action={{ $action = route('admin.pattern.zodiac-delete', $pattern->id) }}>
+                                                <i class="bi bi-x-lg"></i>
                                             </button>
-                                        </a>
-                                        <button type="button" class="btn btn-danger mx-1" data-bs-toggle="modal"
-                                            data-bs-target="#deleteModal{{ $id = $pattern->id }}"
-                                            data-action={{ $action = route('admin.pattern.zodiac-delete', $pattern->id) }}>
-                                            <i class="bi bi-x-lg"></i>
-                                        </button>
-                                    </div>
-                                </td>
+                                        </div>
+                                    </td>
 
-                            </tr>
-                            @include('Includes.admin.modal')
-                        @endforeach
-                    @endif
-                </x-slot>
-                <x-slot name='paginate'>
-                    @include('Includes.admin.paginate', ['paginate' => $zodiacPatterns])
-                </x-slot>
-            </x-admin.table>
+                                </tr>
+                                @include('Includes.admin.modal')
+                            @endforeach
+                        @endif
+                    </x-slot>
+                    <x-slot name='paginate'>
+                        @include('Includes.admin.paginate', ['paginate' => $zodiacPatterns])
+                    </x-slot>
+                </x-admin.table>
+            </div>
         </div>
     </div>
 </x-admin.layout>
