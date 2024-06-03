1d3b71eb7ddd93d94e09188a3898be69299260f5
LamQuangTruong <lqtruong321@gmail.com>
2024-06-03 08:39:59 +0000

    Add route download solar pdf cover

--- horoscope/routes/web_user.php
+++ horoscope/routes/web_user.php
@@ -119,6 +119,7 @@ Route::prefix('download_images')->name('download_images.')->group(static functio
     Route::get('download_sample_pdf',  [BookbindingController::class, 'downloadSamplePdf'])->name('download_sample_pdf');
     // 表紙イメージ（お客様お名前入り）
     Route::get('download_cover_pdf/{design}/{name1}/{name2}',  [BookbindingController::class, 'downloadCoverPdf'])->name('download_cover_pdf');
+    Route::get('download_solar_cover_pdf/{design}/{name1}/{name2}',  [SolarBookbindingController::class, 'downloadCoverPdf'])->name('download_solar_cover_pdf');
 });
 
 // ログイン認証後
