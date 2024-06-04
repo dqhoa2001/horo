e7506dbf093c331178e6f24a7dcd53cb335fb2b3
LamQuangTruong <lqtruong321@gmail.com>
2024-06-04 02:49:54 +0000

    Fix View personal and family solar appraisal offer

--- horoscope/resources/views/components/parts/user/solar_appraisal_apply_baner.blade.php
+++ horoscope/resources/views/components/parts/user/solar_appraisal_apply_baner.blade.php
@@ -1,22 +1,24 @@
 <div class="C-appraisal-banner-block">
-    <div class="C-appraisal-banner__image">
+    <div class="C-solar-appraisal-banner__image">
         <!-- <img src="{{ asset('images/mypage/solar-banner-top-logo.svg') }}" alt=""> -->
         <picture>
-            <source srcset="{{ asset('images/mypage/solar-banner-top-logo.svg') }}"
+            <source srcset="{{ asset('images/mypage/small-solar-return-title.png') }}"
                 media="(max-width: 600px)">
-            <img src="{{ asset('images/mypage/big-solar-banner-top-logo.svg') }}" alt="PERSONAL SOLAR APPRAISAL">
+            <img src="{{ asset('images/mypage/solar-return-title.png') }}" alt="PERSONAL SOLAR APPRAISAL">
         </picture>
     </div>
-    @if(str_contains(Request::url(), 'family_appraisals'))
-        <p class="C-appraisal-banner_sub-title C-appraisal-banner__price-family">［ 家族割引価格 ］</p>
-        <p class="C-appraisal-banner_   _price C-appraisal-banner__price-family">
-            {{ number_format($solar_appraisal->family_price) }}円<span class="C-appraisal-banner__price-family">(税込)</span>
-        </p>
-    @else
-        <p class="C-appraisal-banner__price C-appraisal-banner__price--mt C-solar-appraisal-banner__price-appraisal">
-            {{ number_format($solar_appraisal->price) }}円<span>(税込)</span>
-        </p>
-    @endif
+    <div class="C-solar-appraisal-banner__price-appraisal">
+        @if(str_contains(Request::url(), 'family_appraisals'))
+            <p class="C-appraisal-banner_sub-title">［ 家族割引価格 ］</p>
+            <p class="C-appraisal-banner__price C-solar-appraisal-banner__price-appraisal" >
+                {{ number_format($solar_appraisal->family_price) }}円<span>(税込)</span>
+            </p>
+        @else
+            <p class="C-appraisal-banner__price C-appraisal-banner__price--mt C-solar-appraisal-banner__price-appraisal">
+                {{ number_format($solar_appraisal->price) }}円<span>(税込)</span>
+            </p>
+        @endif
+    </div>
     <p class="C-solar-appraisal-banner__book-price">
         [ 製本オプション + {{ $bookbinding->price_formatted
         }}円<span class="book-price">(税込)</span>]<span class="book-price">　<br class="sp">※製本オプションは2024年2月リリース</span>
@@ -34,7 +36,7 @@
             <div>
                 <div class="C-solar-appraisal-banner__sub-box-image">
                     @if(str_contains(Request::url(), 'family_appraisals'))
-                        <img src="{{ asset('images/mypage/sample-text-family.svg') }}" alt="">
+                        <img src="{{ asset('images/mypage/solar-sample-text.svg') }}" alt="">
                     @else
                         <img src="{{ asset('images/mypage/solar-sample-text.svg') }}" alt="">
                     @endif
@@ -45,7 +47,12 @@
                 <div class="solar-sample-btn-block">
                     <a href="{{ route('user.appraisals.download_sample_pdf') }}">
                         @if(str_contains(Request::url(), 'family_appraisals'))
-                            <img src="{{ asset('images/common/sample-btn-family.svg') }}" alt="">
+                            <!-- <img src="{{ asset('images/common/sample-btn-family.svg') }}" alt=""> -->
+                            <picture>
+                                <source srcset="{{ asset('images/common/small-solar-sample-btn.svg') }}"
+                                    media="(max-width: 500px)">
+                                <img src="{{ asset('images/common/solar-sample-btn.svg') }}" alt="">
+                            </picture>
                         @else
                             <picture>
                                 <source srcset="{{ asset('images/common/small-solar-sample-btn.svg') }}"
@@ -62,9 +69,11 @@
     <div class="C-solar-appraisal-banner__under-btn">
         <!-- <a href="{{ route('user.appraisals.create', ['target_type' => str_contains(Request::url(), 'family_appraisals') ? '2' : '']) }}"> -->
         <a href="{{route('user.check_payment_solar.create')}}">
-        <span class="solar-button">
-                お申し込みはこちら
-            </span>
+            <picture>
+                <source srcset="{{ asset('images/common/small-solar-return-button.png') }}"
+                    media="(max-width: 600px)">
+                <img src="{{ asset('images/common/solar-return-button.png') }}" alt="">
+            </picture>
         </a>
     </div>
 </div>
