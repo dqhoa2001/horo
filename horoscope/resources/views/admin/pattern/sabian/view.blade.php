e7762d1eef7e97c0427a842be13468da0dd68e9f
Hoa <hoa.duong@vj-partner.com>
2024-08-28 08:50:00 +0000

    fix form sabian pattern

--- horoscope/resources/views/admin/pattern/sabian/view.blade.php
+++ horoscope/resources/views/admin/pattern/sabian/view.blade.php
@@ -92,16 +92,6 @@
                 </div>
             </div>
             <div class="row">
-                <label for="test" class="form-label">Test</label>
-                <div class="col-md-12 col-12">
-                    <div class="form-floating mb-3">
-                        <x-admin.text-area name='test' :value= />
-                        <x-admin.label :label="__('form.content_japanese')" name='content_solar' />
-                        <x-admin.error-message name='content_solar' />
-                    </div>
-                </div>
-            </div>
-            <div class="row">
                 <div class="col-12 d-flex justify-content-end">
                     <div class="me-3">
                         <a href="{{ route('admin.pattern.sabian-list') }}" class="btn btn-outline-dark ml-2">一覧へ戻る</a>
