8e64ae9bdea8cd133c467ebcd4d98ea58fae37b5
Hoa <hoa.duong@vj-partner.com>
2024-09-20 07:45:33 +0000

    No.85

--- horoscope/resources/views/user/mypage/popup.blade.php
+++ horoscope/resources/views/user/mypage/popup.blade.php
@@ -58,10 +58,10 @@
 							<div class="Index-banner-wrap">
 								<div class="Index-banner swiper">
 									<div class="Index-banner__inner swiper-wrapper">
-                                        <!-- <figure class="Index-banner__item swiper-slide"> <a href="https://hoshinomai.jp/book-service" target="_blank" rel="noopener noreferrer"><img
-											src="{{ asset('images/mypage/solarreturn01.png') }}" alt=""></a></figure> -->
+                                        <figure class="Index-banner__item swiper-slide"> <a href="https://hoshinomai.jp/book-service" target="_blank" rel="noopener noreferrer"><img
+											src="{{ asset('images/mypage/solarreturn1.png') }}" alt=""></a></figure>
                                         <figure class="Index-banner__item swiper-slide"> <a href="{{ route('user.solar_appraisals.index') }}" rel="noopener noreferrer"><img
-											src="{{ asset('images/mypage/solarreturn02.png') }}" alt=""></a></figure>
+											src="{{ asset('images/mypage/solarreturn2.png') }}" alt=""></a></figure>
                                         <figure class="Index-banner__item swiper-slide"> <a href="https://hoshinomai.jp/book-service" target="_blank" rel="noopener noreferrer"><img
                                             src="{{ asset('images/mypage/my-page_banner.png') }}" alt=""></a></figure>
 										<figure class="Index-banner__item swiper-slide"><a href="{{ route('user.appraisals.index') }}"><img
